0d4fc54fadf2226586145864cb8aaec86037b76d
thoriq farras <thoriqibrahimfarras@gmail.com>
2024-05-12 13:45:55 +0700

    tweak tabel rekap jumlah

--- resources/views/partials/tabelRekapJumlah.blade.php
+++ resources/views/partials/tabelRekapJumlah.blade.php
@@ -3,56 +3,58 @@
     <h5 class="card-title">{{ $titleTableJumlah }}</h5>
 
     <!-- Table with stripped rows -->
-    <table class="table datatable overflow" id="tabel-jumlah-rekap">
+    <table class="table datatable overflow">
       <thead>      
       <tr> 
-          @for ($i = 1; $i <= 5; $i++)
+          {{-- @for ($i = 1; $i <= 5; $i++)
               @if ($i == 1) 
                   <th scope="col" class="x" data-sortable="false">Sumber Dana</th>
               @else
                   <th scope="col" data-sortable="false">Sumber Dana</th>
               @endif
               <th scope="col" data-sortable="false">Jumlah Judul</th>
-          @endfor
+          @endfor --}}
+          <th>Sumber Dana</th>
+          <th>Jumlah {{ $title }} 2024</th>
+          <th>Jumlah {{ $title }} 2023</th>
+          <th>Jumlah {{ $title }} 2022</th>
+          <th>Jumlah {{ $title }} 2021</th>
+          <th>Jumlah {{ $title }} 2020</th>
       </tr>                
       </thead>
       <tbody>
         <tr>
+          <td>Pemberi Dana A</td>
           <td>10</td>
-          <td>Rp100.000.000</td>
-          <td>8</td>
-          <td>Rp100.000.000</td>
-          <td>5</td>
-          <td>Rp25.000.000</td>
-          <td>3</td>
-          <td>Rp50.000.000</td>
-          <td>2</td>
-          <td>Rp10.000.000</td>
+          <td>10</td>
+          <td>10</td>
+          <td>10</td>
+          <td>10</td>
         </tr>
         <tr>
-          <td>10</td>
-          <td>Rp100.000.000</td>
-          <td>8</td>
-          <td>Rp100.000.000</td>
-          <td>5</td>
-          <td>Rp25.000.000</td>
-          <td>3</td>
-          <td>Rp50.000.000</td>
-          <td>2</td>
-          <td>Rp10.000.000</td>
+          <td>Pemberi Dana B</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
         </tr>
         <tr>
-          <td>10</td>
-          <td>Rp100.000.000</td>
+          <td>Pemberi Dana C</td>
           <td>8</td>
-          <td>Rp100.000.000</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+        </tr>        
+        <tr>
+          <td>Pemberi Dana D</td>
           <td>5</td>
-          <td>Rp25.000.000</td>
-          <td>3</td>
-          <td>Rp50.000.000</td>
-          <td>2</td>
-          <td>Rp10.000.000</td>
-        </tr>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+        </tr>        
       </tbody>
     </table>
     <!-- End Table with stripped rows -->
